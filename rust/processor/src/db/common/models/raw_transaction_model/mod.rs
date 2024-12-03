45a0ed5f80a4bdece6c9600a5fcf25de73188423
leo-lambda256 <leo.lim@lambda256.io>
2024-12-03 18:53:49 +0900

    add: transaction example

--- rust/processor/src/db/common/models/raw_transaction_model/mod.rs
+++ rust/processor/src/db/common/models/raw_transaction_model/mod.rs
@@ -1,1 +1,2 @@
 pub mod raw_transactions;
+mod changes;
--- rust/processor/src/db/common/models/raw_transaction_model/raw_transactions.rs
+++ rust/processor/src/db/common/models/raw_transaction_model/raw_transactions.rs
@@ -1,92 +1,64 @@
+use aptos_protos::{
+    transaction::v1::{Transaction as TransactionPB},
+    util::timestamp::Timestamp,
+};
+use bigdecimal::BigDecimal;
+use field_count::FieldCount;
+use serde::{Deserialize, Serialize};
+use crate::db::common::models::default_models::write_set_changes::WriteSetChange;
+use crate::db::common::models::events_models::events::Event;
+use crate::db::common::models::user_transactions_models::signatures::Signature;
+use crate::utils::util::standardize_address;
 
-// {
-//     "version": 123456789,
-//     "hash": "0x1234567890abcdef1234567890abcdef1234567890abcdef1234567890abcdef",
-//     "state_change_hash": "0x1234567890abcdef1234567890abcdef1234567890abcdef1234567890abcdef",
-//     "event_root_hash": "0x1234567890abcdef1234567890abcdef1234567890abcdef1234567890abcdef",
-//     "state_checkpoint_hash": null,
-//     "gas_used": 123,
-//     "success": true,
-//     "vm_status": "Executed successfully",
-//     "type": "user_transaction",
-//     "timestamp_usecs": 1234567890,
-//     "sender": "0x1234567890abcdef1234567890abcdef",
-//     "sequence_number": 42,
-//     "max_gas_amount": 2000,
-//     "gas_unit_price": 100,
-//     "expiration_timestamp_secs": 1234567890,
-  
-//     "signature": {
-//       "type": "ed25519_signature",
-//       "public_key": "0x1234567890abcdef1234567890abcdef1234567890abcdef1234567890abcdef",
-//       "signature": "0x567890abcdef567890abcdef567890abcdef567890abcdef567890abcdef5678"
-//     },
-  
-//     "payload": {
-//       "type": "entry_function_payload",
-//       "function": "0x1::coin::transfer",
-//       "type_arguments": ["0x1::aptos_coin::AptosCoin"],
-//       "arguments": ["0x234567890abcdef1234567890abcdef", 1000]
-//     },
-  
-//     "changes": [{
-//       "address": "0x1234567890abcdef1234567890abcdef",
-//       "state_key_hash": "0x1234567890abcdef1234567890abcdef1234567890abcdef1234567890abcdef",
-//       "data": {
-//         "type": "0x1::coin::CoinStore<0x1::aptos_coin::AptosCoin>",
-//         "data": {
-//           "coin": {"value": 1000},
-//           "frozen": false,
-//           "deposit_events": {
-//             "counter": 1,
-//             "guid": {
-//               "id": {
-//                 "addr": "0x1234567890abcdef1234567890abcdef",
-//                 "creation_num": 2
-//               }
-//             }
-//           },
-//           "withdraw_events": {
-//             "counter": 1,
-//             "guid": {
-//               "id": {
-//                 "addr": "0x1234567890abcdef1234567890abcdef",
-//                 "creation_num": 3
-//               }
-//             }
-//           }
-//         }
-//       },
-//       "type": "write_resource"
-//     }],
-  
-//     "events": [{
-//       "key": "0x000000000000000000000000000000000000000000000000000000000000000a",
-//       "sequence_number": 1,
-//       "type": "0x1::coin::DepositEvent",
-//       "data": {
-//         "amount": 1000
-//       }
-//     }]
-//   }
-
+#[derive(Clone, Deserialize, Debug, Serialize)]
 pub struct RawTransaction {
-    pub type_: String,
     pub version: u64,
     pub hash: String,
     pub state_change_hash: String,
     pub event_root_hash: String,
-    pub state_checkpoint_hash: String,
+    pub state_checkpoint_hash: Option<String>,
     pub gas_used: u64,
     pub success: bool,
     pub vm_status: String,
     pub accumulator_root_hash: String,
-    pub changes: 
+    pub changes: Vec<WriteSetChange>,
+    pub sender: String,
+    pub sequence_number: u64,
+    pub max_gas_amount: u64,
+    pub gas_unit_price: u64,
+    pub expiration_timestamp_secs: u64,
+    pub payload: Option<serde_json::Value>,
+    pub signature: Vec<Signature>,
+    pub events: Vec<Event>,
     pub timestamp: i64,
-    pub epoch: u64,
-    pub block_height: u64,
-    
+    pub type_: String,
 }
 
-
-
+impl RawTransaction {
+    pub fn from_transaction(txn: &TransactionPB) -> (Self) {
+        let info = txn.info.as_ref().unwrap();
+        Self {
+            version: txn.version,
+            hash: standardize_address(
+                hex::encode(info.hash.as_slice()).as_str(),
+            ),
+            state_change_hash: standardize_address(
+                hex::encode(info.state_change_hash.as_slice()).as_str(),
+            ),
+            event_root_hash: standardize_address(
+                hex::encode(info.event_root_hash.as_slice()).as_str(),
+            ),
+            state_checkpoint_hash: info
+                .state_checkpoint_hash
+                .as_ref()
+                .map(|hash| standardize_address(hex::encode(hash).as_str())),
+            gas_used: info.gas_used,
+            success: info.success,
+            vm_status: info.vm_status.clone(),
+            accumulator_root_hash: standardize_address(
+                hex::encode(info.accumulator_root_hash.as_slice()).as_str(),
+            ),
+        }
+    }
+}
+pub type RawTransactionModel = RawTransaction;
